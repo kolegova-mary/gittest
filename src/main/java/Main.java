d0adea8245ec3f25f40e3f8c9b0c3a9606f4ea85
Masha <kolegovamasha@mail.ru>
2021-01-30 16:05:14 +0300

    Added another cool feature!

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("QWE");
         System.out.println("QWE2");
+
+        System.out.println(getSquare(5));
+    }
+
+    static int getSquare(int x){
+        return x*x;
     }
 }
