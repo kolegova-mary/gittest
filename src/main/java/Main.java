14eb8cc20824e0482b4a9cad0548aacc6978dfd0
Masha <kolegovamasha@mail.ru>
2021-01-30 16:00:07 +0300

    Added new cool feature!

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("QWE");
         System.out.println("QWE2");
+
+        int i = 5;
+        System.out.println(i * i);
     }
 }
